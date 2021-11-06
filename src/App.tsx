886ae1812b90d631030fae87b9ab327a2143de26
yuichiyamashita <pv86enier2dqgbm2ngmj@gmail.com>
2021-11-07 06:24:39 +0900

    Add route setting to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { FC } from "react";
 import { Route, Switch, BrowserRouter } from "react-router-dom";
-import { Top } from "./components/pages";
+import { Top } from "./components/pages/top";
+import { Terms } from "./components/pages/terms";
 
-const App: React.FC = () => {
+const App: FC = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Top} />
+        <Route exact path="/terms" component={Terms} />
       </Switch>
     </BrowserRouter>
   );
