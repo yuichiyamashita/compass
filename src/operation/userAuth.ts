916184263d8da08d548ee22c728394f11ddf3ffc
yuichiyamashita <pv86enier2dqgbm2ngmj@gmail.com>
2021-11-12 06:08:41 +0900

    Add firebaseSignInWithEmailLink to userAuth.ts

--- src/operation/userAuth.ts
+++ src/operation/userAuth.ts
@@ -1,9 +1,10 @@
 import {
   //   fetchSignInMethodsForEmail,
   sendSignInLinkToEmail,
-  //   signInWithEmailLink,
-  //   isSignInWithEmailLink,
+  signInWithEmailLink,
+  isSignInWithEmailLink,
 } from "@firebase/auth";
+import { AppDispatch } from "../app/store";
 // import { doc, getDoc, collection } from "@firebase/firestore";
 
 import { firebaseGetAuth } from "../firebase/firebase";
@@ -12,7 +13,7 @@ const auth = firebaseGetAuth();
 // 登録状況確認処理
 
 // 認証メール送信処理
-const callbackUrl = "https://compass-441d8.web.app/login";
+const callbackUrl = "http://localhost:3000/login";
 const actionCodeSettings = {
   url: callbackUrl,
   handleCodeInApp: true,
@@ -32,6 +33,28 @@ export const firebaseSendSignInLinkToEmail = (email: string) =>
       console.log("message: ", errorMessage);
     });
 
-// ログイン完了の処理
+//　認証完了の処理
+export const firebaseSignInWithEmailLink = () => {
+  if (isSignInWithEmailLink(auth, window.location.href)) {
+    let email = window.localStorage.getItem("emailForSignIn");
+    if (!email) {
+      email = window.prompt("Please provide your email for confirmation");
+    } else {
+      signInWithEmailLink(auth, email, window.location.href)
+        .then((result) => {
+          window.localStorage.removeItem("emailForSignIn");
+          console.log("successfuly");
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          console.log("Code:", errorCode);
+          console.log("Message", errorMessage);
+        });
+    }
+  }
+};
 
 // ユーザー情報取得の処理
+
+// パスワード設定の処理
